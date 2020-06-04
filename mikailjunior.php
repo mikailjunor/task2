fa2f6ec716ad3e9c55937b0dbea3cbbf493bb7e7
Mikailjunior <opeyemi072@yahoo.com>
2020-06-04 10:10:45 +0100

    Add files via upload

---
+++ mikailjunior.php
@@ -0,0 +1,9 @@
+<?php
+  $firstname ="Abdul-Hameed";
+  $lastname = "mikail";
+  $id = "HNG-01357";
+  $lang = "PHP";
+  $email = "opeyemi072@yahoomail.com";
+    echo "Hello world, this is $firstname $lastname with HNGi7 ID $id and $email using $lang for stage 2 task" ;
+
+ ?>
